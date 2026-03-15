a7ca2e807feca5948116ae3cef0e8e1ce7eb4e70
AdamKatrenic <adamkatrenic@gmail.com>
2026-03-15 16:11:23 +0100

    CodeWars Remover

---
+++ src/Remover.java
@@ -0,0 +1,26 @@
+public class Remover {
+
+  public static int[] removeSmallest(int[] numbers) {
+
+    if (numbers.length == 0) {
+      return new int[0]; 
+    }
+
+    int minIndex = 0;
+    for (int i = 1; i < numbers.length; i++) {
+      if (numbers[i] < numbers[minIndex]) {
+        minIndex = i;
+      }
+    }
+
+    int[] result = new int[numbers.length - 1];
+    int j = 0;
+    for (int i = 0; i < numbers.length; i++) {
+      if (i != minIndex) {
+        result[j++] = numbers[i];
+      }
+    }
+
+    return result;
+  }
+}
